5e9d014cb77f18ae8e014581fcfee23b9f4bda63
saifulxp <midulahamed999@gmail.com>
2022-08-30 15:28:54 +0600

    catagory modified

--- Admin/manage_catagory.php
+++ Admin/manage_catagory.php
@@ -20,20 +20,79 @@
         <table class="tbl_full">
           <tr>
             <th>S.N.</th>
-            <th>Full Name</th>
-            <th>User Name</th>
+            <th>Title</th>
+            <th>Image</th>
+            <th>Feature</th>
+            <th>Active</th>
             <th>Actions</th>
           </tr>
 
-          <tr>
-            <td>1. </td>
-            <td>Saiful Islam</td>
-            <td>saifulxp</td>
-            <td>
-              <a href="#"class="btn_secoundary">Update Admin</a>
-              <a href="#" class="btn_danger">Delete Admin </a>
-            </td>
-          </tr>
+          <?php
+              $sql="SELECT * FROM tbl_catagory";
+
+              $res=mysqli_query($conn,$sql);
+
+              $count =mysqli_num_rows($res);
+
+              $sn=1;
+              if($count>0)
+              {
+                // We have data in databse
+
+                while($row=mysqli_fetch_assoc($res))
+                {
+                  $id = $row['id'];
+                  $title = $row['title'];
+                  $image_name = $row['image_name'];
+                  $featured = $row['featured'];
+                  $active = $row['active'];
+
+                  ?>
+                    <tr>
+                     <td><?php echo $sn++; ?> </td>
+                     <td><?php echo $title; ?></td>
+
+                     <td>
+                      <?php 
+                      if($image_name!=="")
+                      {
+                        ?>
+
+                        <img src="<?php echo SITEURL;?>images/catagory/<?php echo $image_name;?>" width ="100px">
+                        
+                        <?php
+                      }
+                      else
+                      {
+                        echo "<div class='error'>Image Not Added</div>";
+                      }
+                      ?>
+                    </td>
+
+                     <td><?php echo $featured; ?></td>
+                     <td><?php echo $active; ?></td>
+                     <td>
+                      <a href="#"class="btn_secoundary">Update Catagory</a>
+                      <a href="#" class="btn_danger">Delete Catagory </a>
+                    </td>
+                    </tr>
+                  <?php
+                }
+              }
+              else
+              {
+                // We do not have data
+                ?>
+
+                <tr>
+                    <td colspan="6"><div class="error">No Catagory Added</div></td>
+                </tr>
+
+                <?php
+              }
+          ?>
+
+          
         </table>
       </div>
     </div>
