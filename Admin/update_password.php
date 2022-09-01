52a27e7b58846ed06dda433b78e5c92f81536a25
saifulxp <midulahamed999@gmail.com>
2022-09-01 12:49:39 +0600

    01-sep- changes

--- Admin/update_password.php
+++ Admin/update_password.php
@@ -2,15 +2,14 @@
 <div class="main_content">
   <div class="wrapper">
     <h1>Change Password</h1>
-    <br/> <br/> 
+    <br /> <br />
 
     <?php
-    if(isset($_GET['id']))
-    {
+    if (isset($_GET['id'])) {
       $id = $_GET['id'];
     }
     ?>
-    
+
     <form action="" method="POST">
       <table class="tbl_40">
         <tr>
@@ -23,21 +22,21 @@
         <tr>
           <td>New Password: </td>
           <td>
-            <input type="password" name = "new_password" placeholder="New Password">
+            <input type="password" name="new_password" placeholder="New Password">
           </td>
         </tr>
 
         <tr>
           <td>Confirm Password: </td>
           <td>
-            <input type="password" name = "confirm_password" placeholder= "Confirm Password">
+            <input type="password" name="confirm_password" placeholder="Confirm Password">
           </td>
         </tr>
 
         <tr colspan="2">
           <td>
-            <input type="hidden" name="id" value= "<?php echo $id;?>">
-            <input type="submit" name ="submit" value="Change password" class= "btn_secoundary">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <input type="submit" name="submit" value="Change password" class="btn_secoundary">
           </td>
         </tr>
       </table>
@@ -48,52 +47,40 @@
 <?php include('partial/footer.php'); ?>
 
 <?php
-if(isset($_POST['submit']))
-{
-  $id =$_POST['id'];
-  $current_password=md5($_POST['current_password']);
+if (isset($_POST['submit'])) {
+  $id = $_POST['id'];
+  $current_password = md5($_POST['current_password']);
   $new_password = md5($_POST['new_password']);
   $confirm_password = md5($_POST['confirm_password']);
 
   $sql = "SELECT * FROM tbl_admin WHERE id=$id and password='$current_password'";
-  $res = mysqli_query($conn,$sql);
+  $res = mysqli_query($conn, $sql);
 
-  if($res==TRUE)
-  {
+  if ($res == TRUE) {
     $count = mysqli_num_rows($res);
-    if($count==1)
-    {
-      if($new_password==$confirm_password)
-      {
-        $sql2 ="UPDATE tbl_admin SET password = '$new_password'
+    if ($count == 1) {
+      if ($new_password == $confirm_password) {
+        $sql2 = "UPDATE tbl_admin SET password = '$new_password'
         WHERE id=$id
         ";
 
-        $res2=mysqli_query($conn,$sql2);
+        $res2 = mysqli_query($conn, $sql2);
 
-        if($res2==TRUE)
-        {
-          $_SESSION['pass-cng']="<div class='success'>Password Changed Successfully</div>";
-          header('location: '.SITEURL.'Admin/manage_admin.php');
+        if ($res2 == TRUE) {
+          $_SESSION['pass-cng'] = "<div class='success'>Password Changed Successfully</div>";
+          header('location: ' . SITEURL . 'Admin/manage_admin.php');
+        } else {
+          $_SESSION['pass-cng'] = "<div class='error'>password not changed </div>";
+          header('location: ' . SITEURL . 'Admin/manage_admin.php');
         }
-        else
-        {
-          $_SESSION['pass-cng']="<div class='error'>password not changed </div>";
-          header('location: '.SITEURL.'Admin/manage_admin.php');
-        }
+      } else {
+        $_SESSION['pas-not-match'] = " new and confirm Password Not Match";
+        header('location: ' . SITEURL . 'Admin/manage_admin.php');
       }
-      else
-      {
-        $_SESSION['pas-not-match']=" new and confirm Password Not Match";
-        header('location: '.SITEURL.'Admin/manage_admin.php');
-      }
-    }
-    else
-    {
-      $_SESSION['user-not-found'] ="User Not Found";
-      header('location: '.SITEURL.'Admin/manage_admin.php');
+    } else {
+      $_SESSION['user-not-found'] = "User Not Found";
+      header('location: ' . SITEURL . 'Admin/manage_admin.php');
     }
   }
- 
 }
 ?>
